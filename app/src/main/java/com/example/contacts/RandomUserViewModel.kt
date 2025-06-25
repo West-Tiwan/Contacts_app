f566bf4f38f6691b94e505beb8c7ec6b3c1c96ce
Mohit Singh <mohitsingh@3fat.in>
2025-06-26 00:04:28 +0530

    proper state management

--- app/src/main/java/com/example/contacts/RandomUserViewModel.kt
+++ app/src/main/java/com/example/contacts/RandomUserViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.contacts
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -8,15 +10,33 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 
 class RandomUserViewModel: ViewModel() {
-    var users by mutableStateOf<List<User>>(emptyList())
+    private var _users by mutableStateOf<List<User>>(emptyList())
+
+    private val _searchQuery = mutableStateOf("")
+    val searchQuery: State<String> = _searchQuery
+
     var loading by mutableStateOf(true)
     var error by mutableStateOf<String?>(null)
+
+    val filteredUsers: State<List<User>> = derivedStateOf {
+        if (_searchQuery.value.isBlank()) {
+            _users
+        } else {
+            _users.filter {
+                it.name.first.contains(_searchQuery.value, ignoreCase = true) || it.name.last.contains(_searchQuery.value, ignoreCase = true)
+            }
+        }
+    }
+
+    fun onSearchQueryChange (query: String) {
+        _searchQuery.value = query
+    }
 
     init {
         viewModelScope.launch {
             try {
                 val response = ApiService.api.getUsers()
-                users = response.results
+                _users = response.results
             } catch (e: Exception) {
                 error = "error loading: $e"
             }
